bf2eee1eb1c387e12712c632fbaacc32956516e1
Mikle Zhuravlov <zimboroda@gmail.com>
2019-10-01 15:44:32 +0300

    Get a city and id filter

--- Test area/content.js
+++ Test area/content.js
@@ -33,19 +33,27 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 
-
+// Красаува. Удаляем повторяющиеся мачи и не из Киева
 let arr = document.getElementsByClassName('tf-rd-feed-body_item tf-rd-feed-body_item-new flex');
 let elementInner;
 let elementSkip;
+let arrId =[];
+let element;
 
 for (let i = 0; i < arr.length; i++) {
-    elementInner = document.getElementsByClassName('tf-rd-feed-body_item-user-city')[0];
-    elementSkip = document.getElementsByClassName('tf-rd-feed-body_item-warn')[0];
+elementInner = arr[i].getElementsByClassName('tf-rd-feed-body_item-user-city')[0].innerHTML;// Получить город
+    if (elementInner !== 'Киев') {
+        elementSkip = arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-none tf-rd-feed-body_warn-menu-mut')[0]; //Елемент с 3-мя div
+
+        elementSkip.getElementsByTagName('div')[2].click(); // удаляем
+    }
+    element = arr[i].getElementsByClassName('tf-rd-feed-body_item-warn');//составляем массив айдишников
+    arrId[i] = element[0].getAttribute('data-id'); //выделяем атрибут с id
+    console.log(arrId);
+}
+
+let polova = arr[0].getAttribute('data-id');
+polova.innerHTML
 
 
-    // if (elementInner!=='Киев')  {
-    //     elementSkip = arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-feed-body_warn-menu-mut tf-rd-none')[0];
-    //
-    //
-    // }
- }
+
