ff9a6e7a462f588c2c6f721068a3fdf1023602cf
Mikle Zhuravlov <zimboroda@gmail.com>
2019-10-02 13:24:08 +0300

    Optimise

--- Test area/content.js
+++ Test area/content.js
@@ -1,59 +1,61 @@
-let idArr = document.querySelectorAll('.tf-rd-feed-body_item-new');
-alert(idArr[1]);
 
-function clickOnMail() {
-    let arr = document.querySelectorAll('tbody tr');
-    for (let i = 0; i < arr.length; i++) {
-        if (i % 3 === 0) {
 
-        }
-    }
+
+function checkId (arr,i) {
+    let arrId = [];
+    let itemId = arr.getElementsByClassName(bodyItemClass +'-warn')[0].getAttribute('data-id');
+
+};
+function checkCity(arr,i) {
+    return arr[i].getElementsByClassName(bodyItemClass +'-user-city')[0].innerHTML === 'Киев';
+}
+let bodyItemClass = 'tf-rd-feed-body_item';
+let arr = document.getElementsByClassName(bodyItemClass);
+
+
+
+
+function deleteProfile (arr,i) {
+   return arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-none tf-rd-feed-body_warn-menu-mut')[0].getElementsByTagName('div')[2].click();
 }
 
-clickOnMail();
+function sendMassage(arrElement) {
 
-
-arr = document.querySelectorAll('tbody tr[role="row"]');
-for (let i = 0; i < arr.length; i++) {
-    if (i === 3) {
-        idName = arr[i].getAttribute('id');
-        document.getElementById(idName).click();
-    }
 }
-
-
-arr = document.querySelectorAll('li[data-tooltip="Архивировать"]');
-let className;
-for (let i = 0; i < arr.length; i++) {
-    if (i % 2 === 0) {
-        setInterval();
-        let a = arr[i].getAttribute('class');
-        className = a.split(' ')[0];
-        document.getElementsByClassName(className)[0].click();
-    }
-}
-
 // Красаува. Удаляем повторяющиеся мачи и не из Киева
-let arr = document.getElementsByClassName('tf-rd-feed-body_item tf-rd-feed-body_item-new flex');
+
+
 let elementInner;
-let elementSkip;
-let arrId =[];
+
+
 let element;
 
 for (let i = 0; i < arr.length; i++) {
-elementInner = arr[i].getElementsByClassName('tf-rd-feed-body_item-user-city')[0].innerHTML;// Получить город
+
+    elementInner = arr[i].getElementsByClassName('tf-rd-feed-body_item-user-city')[0].innerHTML;// Получить город
+
     if (elementInner !== 'Киев') {
         elementSkip = arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-none tf-rd-feed-body_warn-menu-mut')[0]; //Елемент с 3-мя div
-
         elementSkip.getElementsByTagName('div')[2].click(); // удаляем
     }
-    element = arr[i].getElementsByClassName('tf-rd-feed-body_item-warn');//составляем массив айдишников
-    arrId[i] = element[0].getAttribute('data-id'); //выделяем атрибут с id
+    arrId[i] = arr[i].getElementsByClassName('tf-rd-feed-body_item-warn')[0].getAttribute('data-id');
+    //arrId[i] = element[0].getAttribute('data-id');
+
     console.log(arrId);
 }
 
-let polova = arr[0].getAttribute('data-id');
-polova.innerHTML
+//arr[i]> проверить город, проверить айди, если чо - удалить.
+
+
+
+
+for (let i = 0; i < arr.length; i++) {
+    if (checkCity(arr[i]) || checkId(arr[i])) {
+        deleteProfile(arr[i]);
+        sendMassage(arr[i]);
+    }
+}
+
 
 
 
---
+++ outside js/content.js
@@ -0,0 +1,5 @@
+
+
+function btnFoo() {
+    alert('button click');
+}
