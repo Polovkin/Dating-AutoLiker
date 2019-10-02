bf777f3e3307260dd100603c1e9185609f96da59
Mikle Zhuravlov <zimboroda@gmail.com>
2019-10-02 16:24:50 +0300

    Work!

--- Test area/content.js
+++ Test area/content.js
@@ -1,60 +1,48 @@
-
-
-
-function checkId (arr,i) {
-    let arrId = [];
-    let itemId = arr.getElementsByClassName(bodyItemClass +'-warn')[0].getAttribute('data-id');
-
-};
-function checkCity(arr,i) {
-    return arr[i].getElementsByClassName(bodyItemClass +'-user-city')[0].innerHTML === 'Киев';
-}
 let bodyItemClass = 'tf-rd-feed-body_item';
-let arr = document.getElementsByClassName(bodyItemClass);
+let mainArr = document.getElementsByClassName(bodyItemClass);
+let arrId = [];
 
-
-
-
-function deleteProfile (arr,i) {
-   return arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-none tf-rd-feed-body_warn-menu-mut')[0].getElementsByTagName('div')[2].click();
-}
-
-function sendMassage(arrElement) {
-
-}
-// Красаува. Удаляем повторяющиеся мачи и не из Киева
-
-
-let elementInner;
-
-
-let element;
-
-for (let i = 0; i < arr.length; i++) {
-
-    elementInner = arr[i].getElementsByClassName('tf-rd-feed-body_item-user-city')[0].innerHTML;// Получить город
-
-    if (elementInner !== 'Киев') {
-        elementSkip = arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-none tf-rd-feed-body_warn-menu-mut')[0]; //Елемент с 3-мя div
-        elementSkip.getElementsByTagName('div')[2].click(); // удаляем
-    }
-    arrId[i] = arr[i].getElementsByClassName('tf-rd-feed-body_item-warn')[0].getAttribute('data-id');
-    //arrId[i] = element[0].getAttribute('data-id');
-
-    console.log(arrId);
-}
-
-//arr[i]> проверить город, проверить айди, если чо - удалить.
-
-
-
-
-for (let i = 0; i < arr.length; i++) {
-    if (checkCity(arr[i]) || checkId(arr[i])) {
-        deleteProfile(arr[i]);
-        sendMassage(arr[i]);
+function refreshArr(arr, arr2) {
+    arr = document.getElementsByClassName(bodyItemClass);
+    for (let i = 0; i < mainArr.length; i++) {
+        arr2[i] = getId(arr, i)
     }
 }
+
+function getId(arr, i) {
+    return arr[i].getElementsByClassName(bodyItemClass + '-warn')[0].getAttribute('data-id');
+}
+
+function checkCity(arr, i) {
+    return arr[i].getElementsByClassName(bodyItemClass + '-user-city')[0].innerHTML === 'Киев';
+}
+
+function deleteProfile(arr, i) {
+    return arr[i].getElementsByClassName('tf-rd-feed-body_warn-menu tf-rd-none tf-rd-feed-body_warn-menu-mut')[0].getElementsByTagName('div')[2].click();
+}
+
+function checkId(arr, i) {
+    refreshArr(arr, arrId);
+    for (let j = arr.length; j > i; j--) {
+        if (getId(arr, i) === arrId[j]) {
+            return true
+        }
+    }
+}
+
+//Наконец-то работает
+function deleteAllProfile(arr) {
+    refreshArr(mainArr, arrId);
+    for (let i = 0; i < arr.length; i++) {
+        if (checkId(arr, i) === true || checkCity(arr,i) !== true) {
+            deleteProfile(arr,i);
+        }
+    }
+}
+
+
+
+
 
 
 
