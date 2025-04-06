6b85c37d07554d69f4459b744f541c1636be5bdd
Mikhail Zhuravlov <zimboroda@gmail.com>
2025-04-06 18:32:24 +0300

    init tailwind

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import AppButton from "../components/AppButton.svelte";
+    import {writable} from "svelte/store";
 
+    const counter = writable(0);
+
+    const increment = () => {
+        counter.update(n => n + 1);
+    }
 </script>
 
 <div class="container mx-auto p-4">
-    <AppButton>
-        Start
-    </AppButton>
+    <button class="w-full" on:click={increment}>
+        Start {$counter}
+    </button>
 </div>
 
 <style>
--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,6 @@
 import Popup from './pages/Popup.svelte'
 import { mount } from "svelte";
+import browser from "webextension-polyfill";
 
 mount(Popup, { target: document.body });
-
 
