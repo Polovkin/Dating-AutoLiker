8a922497715a4398d1cd4a2dca3aa45252e3dbe2
Mikhail Zhuravlov <zimboroda@gmail.com>
2025-04-06 16:04:51 +0300

    init tailwind

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-  console.log("Hello from the popup!");
+    import AppButton from "../components/AppButton.svelte";
+    import {onMount} from "svelte";
+
+    let url: string = 'some url';
+
 </script>
 
-<div>
-  <h1 class="text-2xl text-red-500 font-bold underline">
-    Hello world!1
-  </h1>
-
+<div class="container mx-auto p-4">
+    <AppButton>
+        Start
+    </AppButton>
+    <p>Url: {url}</p>
 </div>
 
 <style>
